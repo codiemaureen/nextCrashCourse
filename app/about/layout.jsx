ab91b2d50605f103692f579e7b1c5cdfd7414acd
Codiemaureen <codiestover.cs@gmail.com>
2025-02-24 10:59:49 -0500

    Adding layouts

---
+++ app/about/layout.jsx
@@ -0,0 +1,10 @@
+const AboutLayout = ({children}) => {
+  return (
+    <div>
+      <h1>This is the About Layout</h1>
+      {children}
+    </div>
+  )
+}
+
+export default AboutLayout
---
+++ app/about/team/page.jsx
@@ -0,0 +1,11 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Teams Page</h1>
+
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  )
+}
+
+export default TeamPage
