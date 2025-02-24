9f66f59df9ef7623b50fb1bd2f5a220802ce4c39
Codiemaureen <codiestover.cs@gmail.com>
2025-02-24 12:37:16 -0500

    Adding react icons

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { FaStar, FaCodeBranch, FaEye} from 'react-icons/fa';
+
 async function fetchRepos(){
   const response = await fetch('https://api.github.com/users/codiemaureen/repos');
   const repos = response.json();
