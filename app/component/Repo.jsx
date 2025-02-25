e7b60f9a9f19e559fc0bb649a4a869b58408956e
Codiemaureen <codiestover.cs@gmail.com>
2025-02-25 17:40:05 -0500

    Adding repo card style

--- app/component/Repo.jsx
+++ app/component/Repo.jsx
@@ -1,17 +1,34 @@
+import Link from "next/link";
+import { FaStar, FaCodeBranch, FaEye} from 'react-icons/fa';
+
 async function fetchRepo(name){
  const response = await fetch(`https://api.github.com/repos/codiemaureen/${name}`);
- await new Promise((resolve) => setTimeout(resolve, 5000));
- const repo = await response.json;
+ await new Promise((resolve) => setTimeout(resolve, 1000));
+ const repo = await response.json();
  return repo;
 }
 
 const Repo = async ({ name }) => {
   const repo = await fetchRepo(name);
-  console.log(repo)
   return (
-    <div>
-      {name}
-    </div>
+    <>
+      <h2>{repo.name}</h2>
+      <p>{repo.description}</p>
+      <div className="card-stats">
+       <div className="card-stat">
+        <FaStar />
+        <span>{repo.stargazers_count}</span>
+       </div>
+       <div className="card-stat">
+        <FaCodeBranch />
+        <span>{repo.forks_count}</span>
+       </div>
+       <div className="card-stat">
+        <FaEye />
+        <span>{repo.watchers_count}</span>
+       </div>
+      </div>
+    </>
   )
 }
 
