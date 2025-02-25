160415cc31d5f5b3d56a77a9e41e886422a04e60
Codiemaureen <codiestover.cs@gmail.com>
2025-02-25 17:47:52 -0500

    Adding back button to card

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,9 +1,12 @@
 import Repo from "@/app/component/Repo";
+import Link from "next/link";
+
 
 async function RepoPage({ params }){
  const {name} = await params;
   return (
    <div className="card">
+     <Link href={`/code/repos`} className="btn btn-back">Back to Respositories</Link>
      <Repo name={name}/>
     </div>
   )
