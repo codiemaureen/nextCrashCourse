43d75320a1359207061538bc9fdf956f9775d01b
Codiemaureen <codiestover.cs@gmail.com>
2025-02-25 19:12:35 -0500

    Adding repo directories

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,6 +1,6 @@
 import Repo from "@/app/component/Repo";
 import Link from "next/link";
-
+import RepoDirs from "@/app/component/RepoDirs";
 
 async function RepoPage({ params }){
  const {name} = await params;
@@ -8,6 +8,7 @@ async function RepoPage({ params }){
    <div className="card">
      <Link href={`/code/repos`} className="btn btn-back">Back to Respositories</Link>
      <Repo name={name}/>
+     <RepoDirs name={name} />
     </div>
   )
 }
