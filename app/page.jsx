10819a19e50fb362e0da449a5a2ded237cf5b9c8
Codiemaureen <codiestover.cs@gmail.com>
2025-02-26 12:44:24 -0500

    Adding course search to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,32 @@
+'use client';
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import LoadingPage from "./loading";
 import Courses from "./component/Courses";
 
 const HomePage = () => {
+  const [courses, setCourses] =  useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () =>  {
+      const res = await fetch('/api/courses');
+      const data = await res.json();
+
+      setCourses(data);
+      setLoading(false);
+    }
+    fetchCourses();
+  }, [])
+
+  if(loading){
+    return <LoadingPage />;
+  }
+
   return (
     <div>
      <h1>Welcome To Codie Codes</h1>
-     <Courses />
-      <ul>
-        <li><Link href='/'>Home</Link></li>
-        <li><Link href='/about'>About</Link></li>
-        <li><Link href='/about/team'>Team</Link></li>
-      </ul>
+     <Courses courses={courses}/>
     </div>
   )
 }
