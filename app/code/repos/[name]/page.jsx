52bc7e2155adb36dd2268b99eb84a76d5055672d
Codiemaureen <codiestover.cs@gmail.com>
2025-02-25 19:19:16 -0500

    Adding suspense boundries

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,14 +1,19 @@
 import Repo from "@/app/component/Repo";
 import Link from "next/link";
 import RepoDirs from "@/app/component/RepoDirs";
+import { Suspense } from "react";
 
 async function RepoPage({ params }){
  const {name} = await params;
   return (
    <div className="card">
      <Link href={`/code/repos`} className="btn btn-back">Back to Respositories</Link>
-     <Repo name={name}/>
-     <RepoDirs name={name} />
+     <Suspense fallback={<div>⌛Loading Repositories... </div>}>
+      <Repo name={name}/>
+     </Suspense>
+     <Suspense fallback={<div>⌛Loading Directories... </div>}>
+      <RepoDirs name={name} />
+     </Suspense>
     </div>
   )
 }
